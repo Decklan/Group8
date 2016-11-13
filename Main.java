1beea1c411ddcf35bc86823d61f44abe584ad948
Cuong Ngo <cuonngo93@gmail.com>
2016-11-12 21:28:04 -0800

    updated provider directory look up. Also updated Makefile

--- Main.java
+++ Main.java
@@ -2,11 +2,20 @@ public class Main {
 
     public static void main(String[] args){
 
-    //Create a new ChocAn object to run
+        //Create a new ChocAn object to run
         ChocAn chocAnTerminal = new ChocAn();
-
         chocAnTerminal.runChocAn();
 
+
+
+
+
+
+
+        /*Test for DataAcess role
+        DataAccess dataAccess = new DataAccess();
+        System.out.println(dataAccess.directoryLookUp());
+        */
 
 
     }
