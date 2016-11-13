888001282c4e140003a53aa828a291c3550dd219
Matt Carnovale <matthew.carnovale@gmail.com>
2016-11-13 15:22:48 -0800

    userDefineRole & checkAnswer
    defineUserRole:
    switch statement handled to allow user to proceed to appropriate menu or get kicked out if they're not a manager or provider
    
    checkAnswer:
    (in the utility class)
    Converted return type to int to allow for a more robust error check. It now handles all types of input: yes, no, or invalid input.

--- ChocAn.java
+++ ChocAn.java
@@ -32,82 +32,175 @@ public class ChocAn extends Utility {
 
 
 
-	/*11.11.16 	 - work on error checking for number vs. string
-				 	+ convert case toLower
-				 	+ see if ther is an isInt function
-				 	+ set user status = to userVerification
-				 - create loop for this prompt
-	 */
-
-
 
 	/* Prompts the user to enter their ID number and calls on the
 	 * userVerification function to verify whether the user is a
 	 * Manager or Provider
-     */
-    public String userLogin() {
+	 */
+	public String userLogin() {
 
-        //Boolean for loop control (true: continue false: break)
-        boolean again = true;
+			//Boolean for loop control (true: continue false: break)
+			int again;
 
-        do {
-            System.out.print("Please Enter Your ID Number:  ");
-            inputUserID = input.nextLine();
+			do {
+				System.out.print("Please Enter Your ID Number:  ");
+				inputUserID = input.findInLine(".{9}");
 
-            //Test that the user's input is an integer.
-            if (testIntegerInput(inputUserID)) {
-                userIDisInteger = Integer.parseInt(inputUserID);
+				//Test that the user's input is an integer.
+				if (testIntegerInput(inputUserID)) {
+					userIDisInteger = Integer.parseInt(inputUserID);
 
-                /*Testing output
-                  System.out.println(DataAccess.userVerification(userIDisInteger));
-                  */
+					/*Testing output
+					System.out.println(DataAccess.userVerification(userIDisInteger));
+                    */
 
-                checkUserStatus = DataAccess.userVerification(userIDisInteger);
+					checkUserStatus = DataAccess.userVerification(userIDisInteger);
 
-                /* If the number is an integer & the user enters an invalid ID number or by chance
-                 * enters a member's ID number, they must be denied by the system and informed that
-                 * the number they have entered will not gain them access to the system.
-                 */
-                if(checkUserStatus.equalsIgnoreCase("invalid") || checkUserStatus.equalsIgnoreCase("suspended")
-                        || checkUserStatus.equalsIgnoreCase("valid"))
-                    System.out.println("This is not a registered access ID in the system.");
-            }
+						/* If the number is an integer & the user enters an invalid ID number or by chance
+						 * enters a member's ID number, they must be denied by the system and informed that
+						 * the number they have entered will not gain them access to the system.
+						 */
+					if(checkUserStatus.equalsIgnoreCase("invalid") || checkUserStatus.equalsIgnoreCase("suspended")
+                        || checkUserStatus.equalsIgnoreCase("member")) {
+						System.out.println("This is not a registered access ID in the system.");
+						checkUserStatus = "invalid";
+					}
 
-            else {
-                checkUserStatus = "invalid";
-            }
+				}
 
-            //If the user's input is invalid, allow them to enter their information again
-            if (checkUserStatus.equalsIgnoreCase("invalid")){
-                System.out.print("Would you like to try again? [y/n]:  ");
-                again = checkAnswer();
-            }
+				else {
+					checkUserStatus = "invalid";
+				}
 
-            else{
-                again = false;
-            }
-
-        }while(again);
-
-        return checkUserStatus;
-    }
+				//If the user's input is invalid, allow them to enter their information again
+				if (checkUserStatus.equalsIgnoreCase("invalid")) {
+					do {
+						System.out.print("Would you like to try again? [y/n]:  ");
+						again = checkAnswer();
+					}while(again == 2);
 
 
-    public void runChocAn (){
+				}
+				else{
+					again = 0;
+				}
 
-        //Prompt the User for their password
-        System.out.println("++++ WELCOME ++++\n\n");
-        userStatus = userLogin();
+				}while(again == 1);
 
-        /*
-        Manager m = new Manager(123213);
-        m.run();
-        Provider p = new Provider(123213);
-        p.run();
+			return checkUserStatus;
+	}
+
+	/* Takes the verified user and defines the role to allow the program to
+	 * proceed to menu screen that corresponds to that user.
+	 * INPUT: NONE
+	 * OUTPUT: True or False
+	 */
+	public void userDefineRole(){
+
+		//Grab that first character to inform the switch statement
+		char statusInformSwitch = userStatus.charAt(0);
+
+		//Handles the user's role
+		switch(statusInformSwitch) {
+
+			//If the user is a manager
+			case 'm':
+
+				endUser = new Manager(userIDisInteger);
+				endUser.run();
+				break;
+
+			//If the user is a provider
+			case 'p':
+
+				endUser = new Provider(userIDisInteger);
+				endUser.run();
+				break;
+
+			//If they're something else at this point, it's time to go buddy
+			default:
+				System.out.println("Level of authorization could not be verified.\nForcing Software Exit.");
+				break;
+		}
+
+		System.out.println("Thank You for using ChocAn.\nGoodbye.");
+
+	}
+
+
+
+
+
+
+	public void runChocAn (){
+
+    System.out.println("++++ WELCOME ++++\n\n");
+
+    //Prompt the User for their password
+	userStatus = userLogin();
+
+    /*Test return value for userStatus
+    System.out.println(userStatus);
+	*/
+
+	//Define the user's role and proceed to the appropriate menu.
+	userDefineRole();
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+	public static void main(String[] argv){
+
+		/*Create new ChocAn client
+		ChocAn chocAnClient = new ChocAn();
+
+
+		chocAnClient.runChocAn();
+		*/	
+
+		/*Test login function
+		chocAnClient.loginTest();
+		*/
+
+
+
+
+
+
+
+
+
+		/*
+         TEST userverificaiton
+        System.out.println(DataAccess.userVerification(123213));
+        System.out.println(DataAccess.userVerification(768921693));
+        System.out.println(DataAccess.userVerification(587857871));
+        System.out.println(DataAccess.userVerification(454419772));
+        System.out.println(DataAccess.userVerification(433671813));
+		*/
+
+        /* TEST addingOrganization
+        DataAccess dataAccess = new DataAccess();
+        boolean a = dataAccess.addOrganization(274419773, "Mike Brown" , "3992 Highland Drive",
+                                   "Milwaukee", "WI", 53202, "member");
         */
 
-        /*Test return value for userStatus
-          System.out.println(userStatus);
-          */
-    }
+        /* TEST Directory LOOK UP
+        DataAccess dataAccess = new DataAccess();
+        String directory = dataAccess.directoryLookUp();
+        System.out.println(directory);
+        */
+        
+
+	}
 }
