61327e7779fc4304556462ccd06360127f554e01
Matt Carnovale <matthew.carnovale@gmail.com>
2016-11-13 15:10:20 -0800

    Update User.Java
    extends Utility class

--- User.java
+++ User.java
@@ -2,7 +2,7 @@
  * This class is the abstract base class for the manager/provider hierarchy.
  */
 
-public abstract class User {
+public abstract class User extends Utility {
     protected int userID;            // holds provider/manager ID
 
     User(int userID) {
