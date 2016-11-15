a451a3114e55fcd8a5f89782e46a2bc72dc277f1
= <vct@pdx.edu>
2016-11-14 20:54:41 -0800

    finishing up createBill

--- Provider.java
+++ Provider.java
@@ -22,11 +22,13 @@ public class Provider extends User {
     }
 
     /* Name: promptID
-     * Input: String name,int length 
-     * Output: int id
+     * Input: String, int
+     * Output: int
      * Description: Prompts for an ID and returns a valid reponse. */
+    
     private int promptID(String name, int length) {
         System.out.print("Enter the " + name + " ID: ");
+        //Read input as String and convert into int
         int idNumber = Integer.parseInt(System.console().readLine());
         if(idNumber == 0 || String.valueOf(idNumber).length() != length) {
             System.out.print("Invalid ID format. Please enter a " + length + "-digit ID number: ");
@@ -35,18 +37,32 @@ public class Provider extends User {
         return idNumber;
     }
 
+    /* Name: promptString
+     * Input: String prompt, int maxLength
+     * Output: String
+     * Description: Prompts for a message and truncates input by length passed as argument.*/
+
     private String promptString(String prompt, int maxLength) {
         System.out.print(prompt);
         String buffer = System.console().readLine();
         return buffer.substring(0, Math.min(maxLength, buffer.length()));
     }
 
-    //Prompts the provider to create the bill
+    /* Name: createBill
+     * Input: None
+     * Output: boolean
+     * Description: Prompts for information required in bill and passes data as args for data access.*/
+    
     public boolean createBill() {
         Date currentDate = new Date(System.currentTimeMillis());
         int serviceID = promptID("service", 6);
+        boolean success = false;
+        while(data.serviceVerification(serviceID) == false) {
+            System.out.print("Invalid service ID!");
+            serviceID = promptID("service", 6);
+        }
         String comment = promptString("Enter a comment (100 max characters): ", 100);
-        data.createBill(887320551, 502891894, serviceID, 1, currentDate, comment); 
+        data.createBill(memberID, userID, serviceID, 1, currentDate, comment); 
         return true;
     }
 
