007b081e0d6047066165d066c809b0fd35ff0eb3
Van Tran <vct@ada.cs.pdx.edu>
2016-11-14 17:58:08 -0800

    began implementing createBill

--- Provider.java
+++ Provider.java
@@ -1,3 +1,4 @@
+import java.sql.Date;
 /**
  * This class contains all the provider methods should contain.
  */
@@ -10,6 +11,7 @@ public class Provider extends User {
     }
 
     //TODO
+    //NOTE: Try and use the promptID function
     private void memberVerification() {
         //Using a do-while loop? ask for member ID and then call
         //dataAccess.userVerification(int id)
@@ -19,29 +21,30 @@ public class Provider extends User {
         //this.memberID = 
     }
 
-
-
-/*********  Driver  **********/
-    public void run() {
-        //memberVerification()
-        System.out.println("Provider driver");
-
-    }
-
-    /*Name: promptID, Input: String, Output: int
-    Description: Prompts for an ID and returns a valid reponse.
-    */
-    protected int promptID(String name) {
+    /* Name: promptID
+     * Input: String name,int length 
+     * Output: int id
+     * Description: Prompts for an ID and returns a valid reponse. */
+    private int promptID(String name, int length) {
         System.out.print("Enter the " + name + " ID: ");
         int idNumber = Integer.parseInt(System.console().readLine());
-        if(idNumber == 0 || String.valueOf(idNumber).length() != 9) {
-            System.out.print("Invalid ID format. Please enter a 9 digit ID number");
+        if(idNumber == 0 || String.valueOf(idNumber).length() != length) {
+            System.out.print("Invalid ID format. Please enter a " + length + "-digit ID number");
             idNumber = Integer.parseInt(System.console().readLine());
         }
         return idNumber;
     }
 
     public boolean createBill() {
+        Date currentDate = new Date(System.currentTimeMillis());
+        int serviceID = promptID("service", 6);
         return true;
+    }
+
+/*********  Driver  **********/
+    public void run() {
+        //memberVerification()
+        System.out.println("Provider driver");
+
     }
 }
