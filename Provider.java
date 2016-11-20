1f44cae541b0250eb00bdc0fbb833ebc5812c0d9
JDemming <jdemming@pdx.edu>
2016-11-20 15:44:04 -0800

    Add files via upload

--- Provider.java
+++ Provider.java
@@ -1,4 +1,6 @@
 import java.sql.Date;
+import sun.misc.Cleaner;
+import java.util.Random;
 /**
  * This class contains all the provider methods should contain.
  */
@@ -12,36 +14,34 @@ public class Provider extends User {
 
     //This method displays the Provider Directory.
     ////The directory stores service names, service codes, and service fees.		
-    public void providerDirectoryDisplay() {		
-        String providerDirectory = data.directoryLookUp();		
-		
-        if(providerDirectory != null) {		
-            System.out.println(providerDirectory);		
-        }		
-        else {		
-            System.out.println("Provider Directory Is Empty");		
-        }		
-		
+    public void providerDirectoryDisplay() {
+        String providerDirectory = data.directoryLookUp();
+
+        if (providerDirectory != null) {
+            System.out.println(providerDirectory);
+        } else {
+            System.out.println("Provider Directory Is Empty");
+        }
+
     }
-    
-    
+
+
     /**
-     *Input : memberID number to verify status
-     *Output: True for member and false for suspended
-     *This function will also set memberID to verifyMember if the user is a member
-    */
+     * Input : memberID number to verify status
+     * Output: True for member and false for suspended
+     * This function will also set memberID to verifyMember if the user is a member
+     */
     public boolean memberVerification(int verifyMember) {
         String memberType;
 
         //returns member, provider, manager or suspended 
         memberType = DataAccess.userVerification(verifyMember);
 
-        if(memberType.equals("member")){
+        if (memberType.equals("member")) {
             memberID = verifyMember;
-            System.out.println("member");
+            System.out.println("member is valid");
             return true;
-        }
-        else if(memberType.equals("suspended")){
+        } else if (memberType.equals("suspended")) {
             System.out.println("Member suspended.");
             return false;
         }
@@ -53,28 +53,86 @@ public class Provider extends User {
      * Input: None
      * Output: boolean
      * Description: Prompts for information required in bill and passes data as args for data access.*/
-    
+
     public boolean createBill() {
         Date currentDate = new Date(System.currentTimeMillis());
-        int serviceID = getIntAnswer("Enter the service id: ", 6);
+        int serviceID;
 
-        while(data.serviceVerification(serviceID) == false) {
+        //Get membership ID number while checking if it's valid or not.
+        do {
+            System.out.println("Enter the member id: ");
+            memberID = input.nextInt();     //CHECK INPUT
+
+        }while(!memberVerification(memberID));           //What if a Provider discoves a member is invalid? do we need an exit statement to avoid getting stuck?
+
+        //Get service ID number while checking if it's valid or not.
+        System.out.println("Enter the service id: ");
+        serviceID = input.nextInt();        //CHECK INPUT
+
+        while (data.serviceVerification(serviceID) == false) {
             System.out.println("Invalid service ID!");
-            serviceID = getIntAnswer("Enter the service id: ", 6);
-        }
+            System.out.println("Enter the service id:");
+            serviceID = input.nextInt();        //CHECK INPUT
 
-        String comment = getStringAnswer("Enter a comment (100 max characters): ", 100);
-        data.createBill(memberID, userID, serviceID, 1, currentDate, comment); 
+        }
+        System.out.println("Enter a comment, up to 100 characters");
+        String comment = input.next();      //CHECK INPUT
+        data.createBill(memberID, userID, serviceID, 1, currentDate, comment);
         return true;
     }
 
-/*********  Driver  **********/
-    public void run() {
-        System.out.println("Provider Terminal Mode");
 
-        int tempID;
+    public int menudisplay() {
+        int MenuChoice = 0;     //stores user's response when making Menu selections
+        System.out.println("Welcome to the Provider Menu.");
+
         do {
-            tempID = getIntAnswer("Please enter member ID: ", 9);
-        } while(!memberVerification(tempID));
+            //Print out menu options to screen
+            System.out.println("###########################################################");
+            System.out.println("## Provider Menu Options:                                 ##");
+            System.out.println("##\t (1) Validate Member                                  ##");
+            System.out.println("##\t (2) Create Bill for Member                           ##");
+            System.out.println("##\t (3) Display Provider Directory                       ##");
+            System.out.println("##\t (4) Exit Provider menu                               ##"); // Not implemented(?)
+            System.out.println("###########################################################");
+
+            System.out.print("Enter your choice (1-4): "); // Prompt manager for a choice
+
+            MenuChoice = input.nextInt();   //Input Provider's choice   //CHECK INPUT
+
+                if(MenuChoice <= 0 || MenuChoice > 4) {
+
+                System.out.println("\n Please make a valid choice!");
+            }
+        } while (MenuChoice <= 0 || MenuChoice > 4);
+        return MenuChoice;
+    }
+
+
+
+
+
+    /*********  Driver  **********/
+    public void run() {
+        int Providerchoice;
+        do {
+            Providerchoice = menudisplay();
+
+            if (Providerchoice == 1) {       //Provider chose to Validate Member
+                System.out.println("Enter the member ID number: ");     //Get the Member's ID number
+                memberID = input.nextInt();         //CHECK INPUT
+               memberVerification(memberID);
+            }
+            if (Providerchoice == 2) {       //Provider chose to Create Bill for Member
+                createBill();
+            }
+            if (Providerchoice == 3) {        //Provider choseto Display Provider directory
+                providerDirectoryDisplay();
+            }
+            if (Providerchoice == 4){
+                return;
+            }
+        }while(Providerchoice != 0);
+        return;
     }
 }
