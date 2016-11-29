6ab48faa6e7843c67d455a3bad42b7692e80d382
sohantamang <tamangsohan@gmail.com>
2016-11-29 10:29:39 -0800

    Started Utility test

---
+++ tests/UtilityTest.java
@@ -0,0 +1,54 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sohan on 11/29/2016.
+ */
+public class UtilityTest {
+
+    @Test
+    public void testReadString() throws Exception {
+
+    }
+
+    @Test
+    public void testReadInt() throws Exception {
+
+    }
+
+    @Test
+    public void testCheckAnswer() throws Exception {
+
+    }
+
+    @Test
+    public void testTestIntegerInput() throws Exception {
+
+    }
+
+    @Test
+    public void testClearScreen() throws Exception {
+
+    }
+
+    @Test
+    public void testWaitForEnter() throws Exception {
+
+    }
+
+    @Test
+    public void testErrorMessage() throws Exception {
+
+    }
+
+    @Test
+    public void testWarningMessage() throws Exception {
+
+    }
+
+    @Test
+    public void testSuccessMessage() throws Exception {
+
+    }
+}
