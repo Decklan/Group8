2c49f3487b79def752a3fb648bb84826d3b391e6
Van <vct@pdx.edu>
2016-11-29 17:07:04 -0800

    isValidMember tested

--- ManagerTest.java
+++ ManagerTest.java
@@ -23,7 +23,12 @@ public class ManagerTest {
     @Test
     public void isValidMember() throws Exception {
         ChocAn = new Manager(433671813);
-
+        assertTrue(ChocAn.isValidMember(638287304)); //Active Member
+        assertTrue(ChocAn.isValidMember(200630841)); //Suspended Member
+        assertFalse(ChocAn.isValidMember(433671813)); //Manager
+        assertFalse(ChocAn.isValidMember(884003881)); //Provider
+        //Active == Suspended == True
+        assertEquals(ChocAn.isValidMember(638287304), ChocAn.isValidMember(200630841));
     }
 
     @Test
