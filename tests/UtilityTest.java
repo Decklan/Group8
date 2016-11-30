98935421ae5322f43545bda1fb89faa7c658740e
sohantamang <tamangsohan@gmail.com>
2016-11-29 16:31:23 -0800

    removed the empty methods

--- tests/UtilityTest.java
+++ tests/UtilityTest.java
@@ -4,25 +4,11 @@ import static org.junit.Assert.*;
 
 
 public class UtilityTest {
-    Utility temp = new Utility();
 
-    @Test
-    public void testReadString() throws Exception {
-
-    }
-
-    @Test
-    public void testReadInt() throws Exception {
-
-    }
-
-    @Test
-    public void testCheckAnswer() throws Exception {
-
-    }
 
     @Test
     public void testTestIntegerInput() throws Exception {
+        Utility temp = new Utility();
         assertEquals(true, temp.testIntegerInput("12365"));
         assertEquals(true, temp.testIntegerInput("123652323"));
         assertEquals(true, temp.testIntegerInput("-12365"));
@@ -30,28 +16,4 @@ public class UtilityTest {
         assertEquals(false, temp.testIntegerInput("daniel sanders"));
     }
 
-    @Test
-    public void testClearScreen() throws Exception {
-
-    }
-
-    @Test
-    public void testWaitForEnter() throws Exception {
-
-    }
-
-    @Test
-    public void testErrorMessage() throws Exception {
-
-    }
-
-    @Test
-    public void testWarningMessage() throws Exception {
-
-    }
-
-    @Test
-    public void testSuccessMessage() throws Exception {
-
-    }
 }
