db96b7774ef3a35935ba0492991720beeaa7cc43
sohantamang <tamangsohan@gmail.com>
2016-11-29 18:02:49 -0800

    Moved the obcject creation to the top

--- tests/ProviderTest.java
+++ tests/ProviderTest.java
@@ -3,11 +3,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ProviderTest {
-    protected Provider providence;
+    protected Provider providence = new Provider(884003881) ;
 
     @Test
     public void testMemberVerification() throws Exception {
-        providence = new Provider(884003881);
         assertEquals(true, providence.memberVerification(644147161)); //test using valid ID
         assertEquals(false, providence.memberVerification(-4562135)); //test using invalid ID
         assertEquals(false, providence.memberVerification(1));        //test using manager's ID
@@ -15,7 +14,6 @@ public class ProviderTest {
 
     @Test
     public void testCreateBill() throws Exception {
-        providence = new Provider(884003881);
         assertEquals(true,providence.data.serviceVerification(175812));
         assertEquals(false,providence.data.serviceVerification(111111));
     }
